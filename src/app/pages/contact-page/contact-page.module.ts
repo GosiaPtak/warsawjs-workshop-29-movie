304777e7d6154382112f17167de07aca17e7f2b5
GosiaPtak <ptakma@gmail.com>
2019-03-06 14:31:27 +0100

    Updates

--- src/app/pages/contact-page/contact-page.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ContactPageComponent } from './components/contact-page.component';
-
-@NgModule({
-  declarations: [ContactPageComponent],
-  imports: [CommonModule]
-})
-export class ContactPageModule {}
--- src/app/pages/search-page/search-page.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SearchPageComponent } from './components/search-page.component';
-
-@NgModule({
-  declarations: [SearchPageComponent],
-  imports: [CommonModule]
-})
-export class SearchPageModule {}
